82c2e109385b5f71f32e42c5390da1ca9694264f
ctac18 <sharmuta73@mail.ru>
2023-06-15 01:50:18 +0300

    Activity + acBat save & back

---
+++ app/src/main/java/com/vssoft/necotut/ui/NewNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.vssoft.necotut.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vssoft.necotut.R
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+    }
+}
